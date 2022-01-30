ce25925666f99acca40a76e077eb106211fcbc83
FelipeValentim <felipevalentimdasilva@gmail.com>
2022-01-30 15:17:26 -0300

    Scroll End

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,29 @@
-const sections = document.querySelectorAll('.js-scroll');
-if (sections.length) {
-    const halfWindow = window.innerHeight * 0.85;
+function scrollAnimInit() {
+    const sections = document.querySelectorAll('.js-scroll');
+    if (sections.length) {
+        const halfWindow = window.innerHeight * 0.85;
 
-    function scrollAnim() {
-        sections.forEach((section) => {
-            const sectionTop = section.getBoundingClientRect().top - halfWindow;
-            if (sectionTop < 0) {
-                section.classList.add('ativo');
-            } else {
-                section.classList.remove('ativo');
-            }
-        });
+        function scrollAnim() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top - halfWindow;
+                if (sectionTop < 0) {
+                    section.classList.add('ativo');
+                } else {
+                    section.classList.remove('ativo');
+                }
+            });
+        }
+        scrollAnim()
+        window.addEventListener('scroll', scrollAnim);
     }
-    scrollAnim()
-    window.addEventListener('scroll', scrollAnim);
+
+    window.onscroll = () => {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            sections.forEach((section) => {
+                section.classList.add('ativo');
+            });
+        }
+    };
 }
+
+scrollAnimInit()
