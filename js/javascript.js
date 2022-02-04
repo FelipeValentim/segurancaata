59ab8833344cf763b3d1985bb4acab77cc9fc293
FelipeValentim <felipevalentimdasilva@gmail.com>
2022-02-04 01:41:55 -0300

    partial anim

--- js/javascript.js
+++ js/javascript.js
@@ -33,7 +33,7 @@ const mains = document.querySelectorAll('.js-menu');
 
 function changeMain(event) {
     event.preventDefault();
-
+    const ativo = document.querySelector('.ativo').getBoundingClientRect().x;
     headerButton.forEach((button) => {
         button.classList.remove('ativo');
     });
@@ -42,15 +42,26 @@ function changeMain(event) {
     const href = event.currentTarget.getAttribute('href');
     const main = document.querySelector(href);
     mains.forEach((main) => {
+        // main.classList.remove('js-menu-ativo-direita');
+        // main.classList.remove('js-menu-ativo-esquerda');
         main.classList.remove('js-menu-ativo');
+
     });
-    // console.log(main);
+    console.log(this.getBoundingClientRect().x, ativo);
     main.classList.add('js-menu-ativo');
+    // if (this.getBoundingClientRect().x > document.querySelector('.ativo').getBoundingClientRect().x) {
+    //     main.classList.add('js-menu-ativo-esquerda');
+    // } else {
+    //     main.classList.add('js-menu-ativo-direita');
+    // }
+
 
 
 }
 
 document.querySelector("#main-contact").classList.add('js-menu-ativo');
+document.querySelector("a[href='#main-contact']").classList.add('ativo');
+
 
 headerButton.forEach((button) => {
     button.addEventListener('click', changeMain);
